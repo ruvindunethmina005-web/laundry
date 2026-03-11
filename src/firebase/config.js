2b9539689633648893856515c182e4974c4a982d
ruvindunethmina005-web <ruvindunethmina005@gmail.com>
2026-03-12 00:58:10 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";  // මේක add කරන්න
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -14,6 +15,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const auth = getAuth(app);  // auth initialize කරන්න
+const auth = getAuth(app);
+const db = getFirestore(app);
 
-export { app, analytics, auth };  // auth එකත් export කරන්න
+export { app, analytics, auth, db };
