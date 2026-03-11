d81f58216f0a741da77d5b84dcd9a546c36228cc
ruvindunethmina005-web <ruvindunethmina005@gmail.com>
2026-03-12 00:33:36 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,16 +2,17 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+  apiKey: "AIzaSyACi3wqYgY-cKjN9yKLX2xk2aoiap4gvBo",
+  authDomain: "accapplication-laundry.firebaseapp.com",
+  projectId: "accapplication-laundry",
+  storageBucket: "accapplication-laundry.firebasestorage.app",
+  messagingSenderId: "195119477461",
+  appId: "1:195119477461:web:01812312c2c08eb053e85c",
+  measurementId: "G-E85LS6L86Q"
 };
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
 export { app, analytics };
+
